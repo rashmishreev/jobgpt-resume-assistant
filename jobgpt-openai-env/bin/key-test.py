c2f57b9d001eb9bdc7e4851c235c0c99be7ffab0
Rashmi Shree Veeraiah <rashmi.v238@gmail.com>
2023-12-02 20:40:07 -0800

    test-connection-request

---
+++ jobgpt-openai-env/bin/key-test.py
@@ -0,0 +1,16 @@
+# from openai import OpenAI
+# import os
+
+# #client = OpenAI()
+# # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# # if you saved the key under a different environment variable name, you can do something like:
+# client = OpenAI(
+#   api_key=os.environ.get("OPENAI_API_KEY_JOBGPT"),
+# )
+
+
+from openai import OpenAI
+client = OpenAI()
+
+file_list = client.files.list()
+print(file_list, sep= "\n")
